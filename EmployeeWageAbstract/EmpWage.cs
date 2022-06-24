c504fbec38e1ebcac4b787129b3005ea0e3adb44
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-06-25 02:11:47 +0530

    [Rajesh] Added : Review Test 2

---
+++ EmployeeWageAbstract/EmpWage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageAbstract
+{
+    public abstract class Employee
+    {
+        public abstract void EmployeeWage();
+        public int EmpPresent = 1;
+        public int FullTime = 1;
+        public int Wage_Per_Hour = 20;
+        public int Full_Day_Hour = 8;
+        public int PartTime_Day_Hour = 4;
+        public int wage = 0;
+
+    }
+
+    public class Wage : Employee
+    {
+        public override void EmployeeWage()
+        {
+            Random EmpCheck = new Random();
+            int value = EmpCheck.Next(0, 2);
+
+            if (value == EmpPresent)
+            {
+                Random TimeCheck = new Random();
+                int WorkingHours = TimeCheck.Next(0, 2);
+                if (WorkingHours == FullTime)
+                {
+                    wage = Wage_Per_Hour * Full_Day_Hour;
+                    Console.WriteLine("Employee Full Time Wage is = " + wage);
+                }
+
+                else
+                {
+                    wage = Wage_Per_Hour * PartTime_Day_Hour;
+                    Console.WriteLine("Employee Part Time Wage is = " + wage);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                Console.WriteLine("Employee Daily Wage is = 0");
+            }
+            
+        }
+    }
+
+    
+        
+        
+            
+        
+
+       
+
+    
+}
---
+++ EmployeeWageAbstract/Program.cs
@@ -0,0 +1,14 @@
+﻿using BasicProgram;
+
+namespace EmployeeWageAbstract
+{
+    class Program
+    {
+        public static void Main(String [] args)
+        {
+            Wage empWage = new Wage();
+            empWage.EmployeeWage();
+
+        }
+    }
+}
